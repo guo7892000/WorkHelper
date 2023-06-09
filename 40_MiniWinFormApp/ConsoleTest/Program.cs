434d4734819bc5eeeb146313c3a11f26fc9fd76b
guohui.huang <guo7892000@126.com>
2023-06-09 23:08:32 +0800

    1.获取变化的文件：新增功能，方便备份修改过的文件。<br> 2.数据Exel公式生成：新增功能，方便生成Excel数据模板中的公式字符。 3.表列字符串生成：去掉【Excel拼接公式字符生成】类型，因该功能已在【数据Exel公式生成】中实现。

---
+++ 40_MiniWinFormApp/ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Breezee.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3*26*26+25;
+            Console.WriteLine(i.ToExcelColumnWord());
+            Console.WriteLine(i.ToUpperWord());
+            Console.ReadKey();
+        }
+    }
+}
--- 40_MiniWinFormApp/ConsoleTest/Properties/AssemblyInfo.cs
+++ 40_MiniWinFormApp/ConsoleTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("工作助手")]
+[assembly: AssemblyTitle("ConsoleTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BreezeeHui")]
-[assembly: AssemblyProduct("Work Helper")]
-[assembly: AssemblyCopyright("Copyright © BreezeeHui 2023")]
+[assembly: AssemblyCompany("Organization")]
+[assembly: AssemblyProduct("ConsoleTest")]
+[assembly: AssemblyCopyright("Copyright © Organization 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("29797a04-3a89-4455-ad02-aad3d818e1a2")]
+[assembly: Guid("f14f5e4d-a7d7-415e-9a10-7eb5d25463e8")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.12.0")]
-[assembly: AssemblyFileVersion("1.2.12.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
