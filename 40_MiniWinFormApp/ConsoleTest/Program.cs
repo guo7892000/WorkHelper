472cb189911724c3db1f88fe50b6560fde9a516e
guohui.huang <guo7892000@126.com>
2023-11-23 23:45:42 +0800

    修正下载最新版本逻辑

--- 40_MiniWinFormApp/ConsoleTest/Program.cs
+++ 40_MiniWinFormApp/ConsoleTest/Program.cs
@@ -53,14 +53,10 @@ namespace ConsoleTest
         private static async void downLoadTet()
         {
             //string sResult = AppUpgradeTool.ReadWebText("https://gitee.com/breezee2000/WorkHelper/blob/master/README.md");
-
+            //注：gitee上发布包占用的空间是有限的。如果超了，发布包发布不上去的，删除之前的一些发布包，再重新发布即可。
             //await AppUpgradeTool.DownloadWebZipAndUnZipAsync(@"https://gitee.com/breezee2000/WorkHelper/releases/download/1.2.24/WorkHelper1.2.24.rar", "E:\\mypeach");
-            await AppUpgradeTool.DownloadWebZipAndUnZipAsync("https://gitee.com/breezee2000/WorkHelper/raw/dev/ReleaseLatest/WorkHelper1.2.36.rar", "E:\\mypeach");
-            //while(new FileInfo(@"E:\mypeach\WorkHelper1.2.36.rar.tmp").Length< 1024 * 1024 * 100)
-            //{
-            //    await AppUpgradeTool.DownloadWebZipAndUnZipAsync(@"https://gitee.com/breezee2000/WorkHelper/raw/master/ReleaseLatest/WorkHelper1.2.36.rar", "E:\\mypeach");
-            //}
-
+            await AppUpgradeTool.DownloadWebZipAndUnZipAsync("https://github.com/guo7892000/WorkHelper/releases/download/1.2.36/WorkHelper1.2.36.rar", "E:\\mypeach");
+            //await AppUpgradeTool.DownloadWebZipAndUnZipAsync("https://gitlab.com/guo7892000/WorkHelper/uploads/1c41da296dca4d999cf8bc08fc06d00e/WorkHelper1.2.36.rar", "E:\\mypeach");
         }
     }
 }
